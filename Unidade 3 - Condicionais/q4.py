dd9dadf9367b2bf25b96b648d31fb20ee56e92e0
Nathan <novadrake76@gmail.com>
2022-07-01 20:05:03 -0300

    question 4 progress

--- Unidade 3 - Condicionais/q4.py
+++ Unidade 3 - Condicionais/q4.py
@@ -1,34 +1,34 @@
+# QUESTÃO INCOMPLETA
 
 color = input()
-p = int(input())
-m = int(input())
-g = int(input())
 
-stock = dict([('p', 10), ('m', 7), ('g', 5)], [('p', 10), ('m', 7), ('g', 5)])
+inputs = {
+    "P("+color+")": 0,
+    "M("+color+")": 0,
+    "G("+color+")": 0,
+}
 
-pStockA = (10)
-mStockA = (7)
-gStockA = (5)
-pStockV = (12)
-mStockV = (5)
-gStockV = (4)
+stock = {
+    "P(A)": 10,
+    "M(A)": 7,
+    "G(A)": 5,
+    "P(V)": 12,
+    "M(V)": 5,
+    "G(V)": 4
+}
 
-if(color == 'A'):
-    globals()[pStock+color] = (pStockA - p)
-    mStockA = (pStockA - m)
-    gStockA = (pStockA - g)
-    for i in [pStockA, mStockA, gStockA, pStockV, mStockV, gStockV]:
-        if(int(i) < 0):
-            print('quantidade não disponível')
-        else:
-            print(i)
+colors = ['A', 'V']
 
-else:
-    pStockV = (12 - p)
-    mStockV = (5 - m)
-    gStockV = (4 - g)
-    for i in [pStockV, mStockV, gStockV, pStockA, mStockA, gStockA]:
-        if(int(i) <= 0):
-            print('quantidade não disponível')
-        else:
-            print(i)
+for i in inputs:
+    inputs[i] = int(input())
+
+sizes = ["P", "M", "G"]
+
+
+for size in sizes:
+    if stock[size+'(' + color + ')'] >= inputs[size + '(' + color + ')']:
+        print(str(size+'(' + color + '):') +
+              str(stock[size+'(' + color + ')'] - inputs[size+'(' + color + ')']))
+    else:
+        print(size+'(' + color + '):' + str(stock[str(size)+'(' + color + ')']
+                                            ) + ' - "Quantidade não disponível"')
