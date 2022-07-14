6e1bd2381c92e6472078c2d303295fccc2df1f70
Nathan <novadrake76@gmail.com>
2022-07-14 18:40:57 -0300

    stuff

--- Unidade 3/PC - Funções/q2.py
+++ Unidade 3/PC - Funções/q2.py
@@ -1,1 +1,19 @@
 # essa tá foda
+
+x0 = int(input())
+y0 = int(input())
+x1 = int(input())
+y1 = int(input())
+
+n = int(input())
+
+objects = []
+
+for i in range(0, n):
+    a = int(input())
+    b = int(input())
+    c = int(input())
+    d = int(input())
+    objects.append({(a, b, c, d): i})
+
+print(objects)
--- Unidade 3/PC - Funções/q3.py
+++ Unidade 3/PC - Funções/q3.py
@@ -14,3 +14,19 @@ M = int(input())
 for x in range(0, M):
     if M != 0:
         list2.append(int(input()))
+
+
+def verify(p):
+    if (p + len(list1)) > len(list2):
+        print('N')
+    else:
+        for i in range(0, list1[-1]):
+            x = list1[i-1]
+            y = list2[(i+p)-1]
+            if x != y:
+                print('N')
+            print('S')
+
+
+for i in list1:
+    verify(i)
